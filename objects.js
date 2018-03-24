2c100de85a7843ca73319157059ce4b560a70813
leo bang <ryanleosb@gmail.com>
2018-03-24 04:42:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,11 @@
-var meals = {};
-var meals = new Object();
+var playlist = new Object({keys: '0'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+};
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+};
